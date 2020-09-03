1ed36414492a160ab301c72d923b96e65cf24cef
andyjohn23 <andrewnick906@gmail.com>
2020-09-03 21:13:44 +0300

    updated files

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-$(document).ready(function(){
-    $("#show1").click(function(){
-       $("#trial2").hide();
-    });
-   });
+let carts = document.querySelectorAll(".add-cart");
+for(let i = 0; i < carts.length; i++){
+    carts[i].addEventListener("click", () => {
+        cartNumbers();
+    })
+}
 
-   $(document).ready(function(){
-    $(".show2").click(function(){
-       $("#trial1").show();
-    });
-   });
+function cartNumbers() {
+    let productNumber = localStorage.getItem("cartNumbers");
+    productNumber = parseInt(productNumber);
+
+    if(productNumber) {
+        localStorage.setItem("cartNumbers", productNumber + 1);
+        document.querySelector(".cart button span").textContent = productNumber + 1;
+    }else{
+        localStorage.setItem("cartNumbers", 1);
+        document.querySelector(".cart button span").textContent = 1;
+    }
+}
