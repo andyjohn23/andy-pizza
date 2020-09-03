0a5ce1282ddee2679bd99b508951b8e237d70e5b
andyjohn23 <andrewnick906@gmail.com>
2020-09-03 19:55:53 +0300

    updated files

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
+$(document).ready(function(){
+    $("#show1").click(function(){
+       $("#trial2").hide();
+    });
+   });
 
-  
+   $(document).ready(function(){
+    $(".show2").click(function(){
+       $("#trial1").show();
+    });
+   });
